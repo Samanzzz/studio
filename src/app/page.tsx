990897444bba68a056b54478f70ee6992b739364
Saman Khadivar <samankhbusiness@gmail.com>
2025-07-06 23:47:14 +0000

    for the photo. i want it to be simple. So get rid of the purple back gro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,14 +45,13 @@ export default function Home() {
                 </div>
             </div>
           </div>
-          <div className="relative group hidden md:block">
-             <div className="absolute -inset-0.5 bg-gradient-to-r from-primary to-purple-600 rounded-full blur-xl opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+          <div className="hidden md:block">
             <Image
               src="/assets/headshot.jpeg"
               alt="Saman Khadivar Headshot"
               width={500}
               height={500}
-              className="relative rounded-full object-cover shadow-2xl border-4 border-secondary"
+              className="rounded-full object-cover"
               priority
             />
           </div>
