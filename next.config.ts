26419cd42e149c810ab20c8712bb6bb52284c1c7
Saman Khadivar <samankhbusiness@gmail.com>
2025-07-06 22:08:26 +0000

    it doesnt who in my github. please make a index.html that would run the

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-
-  // Note: Before deploying to GitHub Pages, you must update the following
-  // configuration to match your repository name. If your repository is at
-  // https://github.com/<username>/<repo-name>, then you should set:
-  //
-  // basePath: '/<repo-name>',
-  //
-  // For more information, see:
-  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#github-pages
   
   images: {
     unoptimized: true, // Required for static export to work with next/image
